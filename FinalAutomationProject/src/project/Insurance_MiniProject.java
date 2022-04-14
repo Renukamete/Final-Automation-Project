bea89e6b8bb441913be77214b8be9f6279ea1e8b
renuk <renuk@DESKTOP-OFS1BHR>
2022-04-14 19:26:14 +0530

    This is my updated commit

--- FinalAutomationProject/src/project/Insurance_MiniProject.java
+++ FinalAutomationProject/src/project/Insurance_MiniProject.java
@@ -174,7 +174,77 @@ public void smokeTesting() throws InterruptedException
 		
 
 	}
-			 
+		public void f() throws Exception 
+		 
+		 {
+				
+				Select s;  // GLOBAL DECLARATION
+				driver.manage().window().maximize();
+				
+				//URL
+				driver.get("https://blazedemo.com");
+				Thread.sleep(2000);
+				
+				//DROP DOWN ----> (DEPARTURE CITY) ; TAGNAME MUST BE <SELECT>
+				//STEP 1 : CREATE AN OBJECT OF THE SELECT CLASS
+				s=new Select(driver.findElement(By.xpath("/html/body/div[3]/form/select[1]")));
+				//STEP 2 : SELECT THE OPTION
+				s.selectByIndex(5);
+				Thread.sleep(2000);
+				
+
+				//DROP DOWN ----> (DESTINATION CITY) ; TAGNAME MUST BE <SELECT>
+				//STEP 1 : CREATE AN OBJECT OF THE SELECT CLASS
+				s=new Select(driver.findElement(By.xpath("/html/body/div[3]/form/select[2]")));
+				//STEP 2 : SELECT THE OPTION
+				s.selectByVisibleText("New York");
+				Thread.sleep(2000);
+				
+				// FIND FLIGHTS BUTTON
+				driver.findElement(By.xpath("/html/body/div[3]/form/div/input")).click();
+				Thread.sleep(2000); 
+				
+				// choose this flight 
+				driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr[1]/td[1]/input")).click();
+				Thread.sleep(2000);
+		//Enter name for ticket 
+				driver.findElement(By.xpath("//*[@id=\"inputName\"]")).sendKeys("Peter Dinklage");
+				Thread.sleep(2000);
+		//Enter Address for ticket 
+				driver.findElement(By.xpath("//*[@id=\"address\"]")).sendKeys("786 Belgum Road");
+				Thread.sleep(2000);
+		//Enter City for ticket
+				driver.findElement(By.xpath("//*[@id=\"city\"]")).sendKeys("Newyork");
+				Thread.sleep(2000);
+		//Enter State 
+				driver.findElement(By.xpath("//*[@id=\"state\"]")).sendKeys("New Jersey");
+				Thread.sleep(2000);
+		//Enter Zip code
+				driver.findElement(By.xpath("//*[@id=\"zipCode\"]")).sendKeys("08701");
+				Thread.sleep(2000);
+		//select card type
+				s=new Select(driver.findElement(By.xpath("//*[@id=\"cardType\"]")));
+				s.selectByIndex(0);
+				Thread.sleep(2000);
+//			Enter Credit Card Number
+				driver.findElement(By.xpath("//*[@id=\"creditCardNumber\"]")).sendKeys("4012888888881881");
+				Thread.sleep(2000);
+		//Enter Month
+				driver.findElement(By.xpath("//*[@id=\"creditCardMonth\"]")).sendKeys("02");
+				Thread.sleep(2000);
+		//Enter Year
+				driver.findElement(By.xpath("//*[@id=\"creditCardYear\"]")).sendKeys("2020");
+				Thread.sleep(2000);
+		//Enter Name on card
+				driver.findElement(By.xpath("//*[@id=\"nameOnCard\"]")).sendKeys("Peter Dinklage");
+				Thread.sleep(2000);
+		//Click for Check Box
+				driver.findElement(By.xpath("//*[@id=\"rememberMe\"]")).click();
+				Thread.sleep(2000);
+		//Click for Submit Button
+				driver.findElement(By.xpath("/html/body/div[2]/form/div[11]/div/input")).click();
+				Thread.sleep(2000);
+		 }
 		
 			
 		@AfterTest
